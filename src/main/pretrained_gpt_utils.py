7cc2a3bf44a7c7d7c6d052998f8f66efd65b39dc
Vinay Kumar <imflash217@gmail.com>
2025-06-24 22:44:09 +0000

    utility functions to download and load pretrained weights

---
+++ src/main/pretrained_gpt_utils.py
@@ -0,0 +1,123 @@
+import json
+import os
+import urllib.request
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from src.config import config as cfg
+
+
+def attempt_download(download_url, destination):
+    with urllib.request.urlopen(download_url) as response:
+        # Get the total file size from headers, defaulting to 0 if not present
+        file_size = int(response.headers.get("Content-Length", 0))
+
+        # Check if file exists and has the same size
+        if os.path.exists(destination):
+            file_size_local = os.path.getsize(destination)
+            if file_size == file_size_local:
+                print(f"File already exists and is up-to-date: {destination}")
+                return True  # Indicate success without re-downloading
+
+        block_size = 1024  # 1 KB
+
+        # Initialize the progress bar with total file size
+        progress_bar_description = os.path.basename(download_url)
+        with tqdm(
+            total=file_size,
+            unit="iB",
+            unit_scale=True,
+            desc=progress_bar_description,
+        ) as progress_bar:
+            with open(destination, "wb") as file:
+                while True:
+                    chunk = response.read(block_size)
+                    if not chunk:
+                        break
+                    file.write(chunk)
+                    progress_bar.update(len(chunk))
+        return True
+
+
+def download_file(url, destination, backup_url=None):
+    try:
+        if attempt_download(url, destination):
+            return
+    except (urllib.error.HTTPError, urllib.error.URLError):
+        if backup_url is not None:
+            print(f"Primary URL ({url}) FAILED. Attempting backup URL: {backup_url}")
+            try:
+                if attempt_download(backup_url, destination):
+                    return
+            except urllib.error.HTTPError:
+                pass
+
+        # If we reach here, both attempts have failed
+        error_message = (
+            f"FAILED to download from both primary URL ({url})"
+            f"{' and backup URL (' + backup_url + ')' if backup_url else ''}."
+        )
+        print(error_message)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def load_gpt2_params_from_tf_ckpt(ckpt_path, settings):
+    # Initialize parameters dictionary with empty blocks for each layer
+    params = {"blocks": [{} for _ in range(settings["n_layer"])]}
+
+    # Iterate over each variable in the checkpoint
+    for name, _ in tf.train.list_variables(ckpt_path):
+        # Load the variable and remove singleton dimensions
+        variable_array = np.squeeze(tf.train.load_variable(ckpt_path, name))
+
+        # Process the variable name to extract relevant parts
+        variable_name_parts = name.split("/")[1:]  # Skip the 'model/' prefix
+
+        # Identify the target dictionary for the variable
+        target_dict = params
+        if variable_name_parts[0].startswith("h"):
+            layer_number = int(variable_name_parts[0][1:])
+            target_dict = params["blocks"][layer_number]
+
+        # Recursively access or create nested dictionaries
+        for key in variable_name_parts[1:-1]:
+            target_dict = target_dict.setdefault(key, {})
+
+        # Assign the variable array to the last key
+        last_key = variable_name_parts[-1]
+        target_dict[last_key] = variable_array
+
+    return params
+
+
+def download_and_load_gpt2(model_size, models_dir):
+    # Validate model size
+    allowed_sizes = cfg.ALLOWED_GPT_SIZES
+    if model_size not in allowed_sizes:
+        raise ValueError(f"Model size not in {allowed_sizes}")
+
+    # Define paths
+    model_dir = os.path.join(models_dir, model_size)
+    base_url = cfg.BASE_GPT_WEIGHTS_URL
+    backup_base_url = cfg.BACKUP_GPT_WEIGHTS_URL
+    filenames = cfg.PRETRAINED_FILES_TO_DOWNLOAD
+
+    # Download files
+    os.makedirs(model_dir, exist_ok=True)
+    for filename in filenames:
+        file_url = os.path.join(base_url, model_size, filename)
+        backup_url = os.path.join(backup_base_url, model_size, filename)
+        file_path = os.path.join(model_dir, filename)
+        download_file(file_url, file_path, backup_url)
+
+    # Load settings and params
+    tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+    settings = json.load(
+        open(os.path.join(model_dir, "hparams.json"), "r", encoding="utf-8")
+    )
+    params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
+
+    return settings, params
